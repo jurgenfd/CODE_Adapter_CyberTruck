ca33e578c2eca4ef6be4b0eeead0620e638492e8
Jurgen Doreleijers <jurgenfd@gmail.com>
2023-12-12 12:42:49 +0100

    Add project files.

---
+++ CODE_Adapter_CyberTruck/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace CODE_Adapter_CyberTruck
+{
+    internal class Car
+    {
+        public int SpeedKpH { get; internal set; }
+
+        internal void Log()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Move(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CODE_Adapter_CyberTruck/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CODE_Adapter_CyberTruck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.SpeedKpH = 120;
+            car.Move(45);
+            car.Log();
+            Console.ReadKey();
+        }
+    }
+}
